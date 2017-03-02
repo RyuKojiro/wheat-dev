42260f3011c36e0fa02b3f7cffd3be28f7104364
Daniel Loffgren <daniel@loffgren.org>
2017-03-02 15:24:45 -0800

    shasm expects exclaimation points for comments

--- counter.s
+++ counter.s
@@ -1,25 +1,25 @@
 .text
 
 main:
-	mov #0, r0               ; Reset r0 (counter)
+	mov #0, r0                 ! Reset r0 (counter)
 loop:
-	bsr displayEntireNumber    ; Display the number
-	nop                        ; Branch Delay Slot
-	add #1, r0                 ; Increment the number
-	mov.l loop, r1             ; Load r1 for jump
-	jmp @r1                    ; Repeat
-	nop                        ; Branch Delay Slot
+	bsr displayEntireNumber    ! Display the number
+	nop                        ! Branch Delay Slot
+	add #1, r0                 ! Increment the number
+	mov.l loop, r1             ! Load r1 for jump
+	jmp @r1                    ! Repeat
+	nop                        ! Branch Delay Slot
 
 displayEntireNumber:
-	mov.l r1, @-r15            ; Push r1 onto the stack
+	mov.l r1, @-r15            ! Push r1 onto the stack
 
-	mov #7, r2                 ; r2 will be our upper limit
+	mov #7, r2                 ! r2 will be our upper limit
 displayDigit:
 	
-	cmp/gt r1, r2               ; Are we done with the 8th digit yet?
-	bt displayDigit            ; If not, go to the next digit
-	add #1, r1                 ; Delayed Branch - Increment the digit count
+	cmp/gt r1, r2              ! Are we done with the 8th digit yet?
+	bt displayDigit            ! If not, go to the next digit
+	add #1, r1                 ! Delayed Branch - Increment the digit count
 
-	mov.l @r15+, r1            ; Pop the stack
+	mov.l @r15+, r1            ! Pop the stack
 	rts
 	nop
