86b0ab5757795b4c3ce4d65adf7e4d4c53c273d3
Daniel Loffgren <daniel@loffgren.org>
2017-03-01 22:03:00 -0800

    Initial import

---
+++ counter.s
@@ -0,0 +1,24 @@
+.org 0x08000000
+
+main:
+	mov.l #0, r0
+loop:
+	bsr displayEntireNumber    ; Display the number
+	nop                        ; Branch Delay Slot
+	add #1, r0                 ; Increment the number
+	mov.l loop, r1             ; Load r1 for jump
+	jmp @r1                    ; Repeat
+	nop                        ; Branch Delay Slot
+
+displayEntireNumber:
+	mov.l r1, @-r15            ; Push r1 onto the stack
+
+displayDigit:
+	
+	cmp/gt r1, 7               ; Are we done with the 8th digit yet?
+	bt displayDigit            ; If not, go to the next digit
+	add #1, r1                 ; Delayed Branch - Increment the digit count
+
+	mov.l @r15+, r1            ; Pop the stack
+	rts
+	nop
