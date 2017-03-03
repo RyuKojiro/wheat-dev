279faaaa51b90f422c8c5869c9b82d081067cff4
Daniel Loffgren <daniel@loffgren.org>
2017-03-02 23:10:43 -0800

    Whitespace

--- counter.s
+++ counter.s
@@ -16,18 +16,18 @@ displayEntireNumber:
 		! r3 = digit address in memory
 		! r4 = the character to print
 		! r5 = temporary relative
-	mov #0, r1                 ! r1 will be our digit iterator
-	mov #7, r2                 ! r2 will be our upper limit
+	mov #0, r1                       ! r1 will be our digit iterator
+	mov #7, r2                       ! r2 will be our upper limit
 displayDigit:
 	mov.l @(ledDataOffset,pc), r3    ! Get the address to the offset
-	shll r1                    ! r1 *= 2
-	add r1, r3                 ! Add the digit number (*2) to the offset
-	shlr r1                    ! r1 /= 2
-	mov #'[', r4               ! Calculate the character for the digit
-	mov.b r4, @r3              ! Write character to display digit
-	cmp/gt r1, r2              ! Are we done with the 8th digit yet?
-	add #1, r1                 ! Delayed Branch - Increment the digit count
-	bt displayDigit            ! If not, go to the next digit
+	shll r1                          ! r1 *= 2
+	add r1, r3                       ! Add the digit number (*2) to the offset
+	shlr r1                          ! r1 /= 2
+	mov #'[', r4                     ! Calculate the character for the digit
+	mov.b r4, @r3                    ! Write character to display digit
+	cmp/gt r1, r2                    ! Are we done with the 8th digit yet?
+	add #1, r1                       ! Delayed Branch - Increment the digit count
+	bt displayDigit                  ! If not, go to the next digit
 	rts
 	nop
 
