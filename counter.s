4571d6b59bbaeb1f3d8584ba0ec999da7f79d828
Daniel Loffgren <daniel@loffgren.org>
2017-03-02 22:45:09 -0800

    A bit closer to the display

--- counter.s
+++ counter.s
@@ -14,15 +14,24 @@ displayEntireNumber:
 		! r2 = digit max constant
 		! r3 = digit address in memory
 		! r4 = the character to print
+		! r5 = temporary relative
 
 	mov #7, r2                 ! r2 will be our upper limit
 displayDigit:
-	mov r1, r3                 ! Put the target digit location in r3
-	shll r3                    ! r3 *= 2
-
+	mov.l ledDataOffset, r5    ! Get the address to the offset
+	mov.l @r5, r3              ! Set r3 to the offset
+	shll r1                    ! r1 *= 2
+	add r1, r3                 ! Add the digit number (*2) to the offset
+	shlr r1                    ! r1 /= 2
+	mov #'[', r4               ! Calculate the character for the digit
+	mov.b r4, @r3              ! Write character to display digit
 	cmp/gt r1, r2              ! Are we done with the 8th digit yet?
 	bt displayDigit            ! If not, go to the next digit
 	add #1, r1                 ! Delayed Branch - Increment the digit count
 
 	rts
 	nop
+
+.align 2
+ledDataOffset:
+.long 0xBA202070
