510fefb0f95d3b06f7e1dec5692251e5f390a145
Daniel Loffgren <daniel@loffgren.org>
2017-03-04 12:13:38 -0800

    Reordered for implicit declaration

---
+++ ccounter.c
@@ -0,0 +1,29 @@
+static void lcdDisplayCharacter(int digit, char c) {
+	char *lcdStorage = (char *)0xBA202070;
+	lcdStorage[digit*2] = c;
+}
+
+static char tohexchar(int x) {
+	if(x < 0xA) {
+		return x + '0';
+	}
+	else {
+		return x + 'A';
+	}
+}
+
+static void lcdDisplayNumber(long number) {
+	for(int i = 0; i < 8; i++) {
+		int d = (number >> (i * 4)) & 0xF;
+		lcdDisplayCharacter(8-i, tohexchar(d));
+	}
+}
+
+int main() {
+	for(long i = 0;; i++) {
+		lcdDisplayNumber(i);
+	}
+
+	return 0;
+}
+
