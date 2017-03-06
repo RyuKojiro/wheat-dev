75512ea17c947310b954488e4330b1e5c310bf29
Daniel Loffgren <daniel@loffgren.org>
2017-03-06 00:58:06 -0800

    Make sure to preserve the procedure register

--- counter.s
+++ counter.s
@@ -16,6 +16,7 @@ displayEntireNumber:
 		! r3 = digit address in memory
 		! r4 = the character to print
 		! r5 = temporary relative
+	sts.l pr, @-r15
 	mov #0, r1                 ! r1 will be our digit iterator
 	mov #7, r2                 ! r2 will be our upper limit
 displayDigit:
@@ -29,6 +30,7 @@ displayDigit:
 	add #1, r1                 ! Increment the digit count
 	cmp/hs r1, r2              ! Are we done with the 8th digit yet?
 	bt displayDigit            !   If not, go to the next digit
+	lds.l @r15+, pr
 	rts
 	nop
 
@@ -43,7 +45,7 @@ getCurrentDigit:
 		! r6 = the current nibble
 		! r4 = the character to print
 		! r8 = hex alpha limit
-
+	sts.l pr, @-r15
 	mov #0, r7                 ! Reset the shift iterator
 	mov r0, r6                 ! Put the number into the nibble register
 	cmp/hi r7, r1              ! Is there more to shift?
@@ -67,5 +69,6 @@ itsAlpha:
 	add #'A' - 0xA, r6         ! Normalize to an alpha character
 digitDone:
 	mov r6, r4
+	lds.l @r15+, pr
 	rts
 	nop
