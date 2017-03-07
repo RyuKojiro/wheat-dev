fa2e4427035eeaadf28c48b08d896786ed634112
Daniel Loffgren <daniel@loffgren.org>
2017-03-07 01:44:27 -0800

    Whitespace

--- counter.s
+++ counter.s
@@ -2,12 +2,12 @@
 
 main:
 		! r10 = the entire number to print
-	mov #0, r10                 ! Reset r10 (counter)
+	mov #0, r10                ! Reset r10 (counter)
 loop:
 	bsr displayEntireNumber    ! Display the number
 	nop                        ! Empty Branch Delay Slot
 	bra loop                   ! Repeat Unconditionally
-	add #1, r10                 ! ⤷ Increment the number
+	add #1, r10                ! ⤷ Increment the number
 
 displayEntireNumber:
 		! r10 = the entire number to print
