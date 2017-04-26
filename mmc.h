d00a1c85381161c98abd80f42916e1e09238030b
Daniel Loffgren <daniel@loffgren.org>
2017-04-26 00:21:23 -0700

    Some tabs snuck in

--- mmc.h
+++ mmc.h
@@ -1,12 +1,12 @@
 
 #define MMCIF_BASE 0xffe60000
 
-#define MMCIF_CMDR0	  (*(volatile uint8_t  *)(MMCIF_BASE + 0x00)) /* Command Register 0 */
-#define MMCIF_CMDR1	  (*(volatile uint8_t  *)(MMCIF_BASE + 0x01)) /* Command Register 1 */
-#define MMCIF_CMDR2	  (*(volatile uint8_t  *)(MMCIF_BASE + 0x02)) /* Command Register 2 */
-#define MMCIF_CMDR3	  (*(volatile uint8_t  *)(MMCIF_BASE + 0x03)) /* Command Register 3 */
-#define MMCIF_CMDR4	  (*(volatile uint8_t  *)(MMCIF_BASE + 0x04)) /* Command Register 4 */
-#define MMCIF_CMDR5	  (*(volatile uint8_t  *)(MMCIF_BASE + 0x05)) /* Command Register 5 */
+#define MMCIF_CMDR0   (*(volatile uint8_t  *)(MMCIF_BASE + 0x00)) /* Command Register 0 */
+#define MMCIF_CMDR1   (*(volatile uint8_t  *)(MMCIF_BASE + 0x01)) /* Command Register 1 */
+#define MMCIF_CMDR2   (*(volatile uint8_t  *)(MMCIF_BASE + 0x02)) /* Command Register 2 */
+#define MMCIF_CMDR3   (*(volatile uint8_t  *)(MMCIF_BASE + 0x03)) /* Command Register 3 */
+#define MMCIF_CMDR4   (*(volatile uint8_t  *)(MMCIF_BASE + 0x04)) /* Command Register 4 */
+#define MMCIF_CMDR5   (*(volatile uint8_t  *)(MMCIF_BASE + 0x05)) /* Command Register 5 */
 #define MMCIF_CMDSTRT (*(volatile uint8_t  *)(MMCIF_BASE + 0x06)) /* Command Start Register */
 #define MMCIF_OPCR    (*(volatile uint8_t  *)(MMCIF_BASE + 0x0a)) /* Operation Control Register */
 #define MMCIF_CSTR    (*(volatile uint8_t  *)(MMCIF_BASE + 0x0b)) /* Card Status Register */
