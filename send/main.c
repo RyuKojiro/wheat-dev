8c849eacb2aacd40987bfc890b48e42f2e5a85e7
Daniel Loffgren <daniel@loffgren.org>
2017-05-13 18:59:18 -0700

    Added some getopts

--- send/main.c
+++ send/main.c
@@ -1,9 +1,15 @@
 #include <err.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sysexits.h>
 #include <termios.h>
 #include <unistd.h>
+
+static void usage() {
+	fprintf(stderr, "usage");
+	exit(EX_USAGE);
+}
 
 static void configure(const int fd, const int baud) {
 	struct termios tty = {0};
@@ -20,22 +26,38 @@ static void configure(const int fd, const int baud) {
 	}
 }
 
-int main(int argc, const char * const argv[]) {
+static void send(int in, int out) {
+}
+
+int main(int argc, char * const argv[]) {
 	/*
 	 * The expected usage is something along the lines of:
 	 *     send /dev/sttynode < file_to_send
 	 */
 
+	int baud = 115200;
+	int ch;
+	while ((ch = getopt(argc, argv, "b:")) != -1) {
+		switch (ch) {
+			case 'b': {
+				baud = atoi(optarg);
+			} break;
+			case '?':
+			default: {
+				usage();
+			}
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
 	if (argc < 2) {
 		errx(EX_USAGE, "You must supply the serial device node as an argument");
 	}
 
-	const int baud = 115200;
 	const int fd = open(argv[argc-1], O_RDWR | O_NOCTTY | O_SYNC);
-
 	configure(fd, baud);
-
-
+	send(fileno(stdin), fd);
 	close(fd);
 
 	return EX_OK;
