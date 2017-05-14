daf993279dbbffaa84b903a6c4a3677cd9e4d804
Daniel Loffgren <daniel@loffgren.org>
2017-05-14 01:32:32 -0700

    Make sure to take in the actual file as an argument, also added some printing of the send status

--- send/main.c
+++ send/main.c
@@ -10,7 +10,7 @@
 #define BLOCK_SIZE 512
 
 __attribute__((noreturn)) static void usage(void) {
-	fprintf(stderr, "usage: send [-b baud] /dev/node < input\n");
+	fprintf(stderr, "usage: send [-b baud] -s /dev/node input ...\n");
 	exit(EX_USAGE);
 }
 
@@ -33,9 +33,23 @@ static void send(FILE *in, const int out) {
 	struct timespec start;
 	clock_gettime(CLOCK_MONOTONIC, &start);
 
+	size_t totalSize = 0;
+	int rate = 0;
+	int percent = 0;
+	int timeRemaining = 0;
 	char buf[BLOCK_SIZE];
+	off_t offset = 0;
 	size_t bytesRead;
 	while((bytesRead = fread(buf, BLOCK_SIZE, 1, in))) {
+		offset += bytesRead;
+		fprintf(stderr, "%#08llx - %lld/%lu bytes - %d bytes/sec - %u%% - %u remaining\r",
+				offset + 0x80000000,
+				offset,
+				totalSize,
+				rate,
+				percent,
+				timeRemaining
+				);
 		write(out, buf, bytesRead);
 	}
 }
@@ -43,15 +57,19 @@ static void send(FILE *in, const int out) {
 int main(int argc, char * const argv[]) {
 	/*
 	 * The expected usage is something along the lines of:
-	 *     send /dev/sttynode < file_to_send
+	 *     send -b 9600 -s /dev/sttynode file_to_send ...
 	 */
 
 	speed_t baud = 115200;
 	int ch;
+	const char *node = NULL;
 	while ((ch = getopt(argc, argv, "b:")) != -1) {
 		switch (ch) {
 			case 'b': {
 				baud = strtoul(optarg, NULL, 10);
+			} break;
+			case 's': {
+				node = optarg;
 			} break;
 			case '?':
 			default: {
@@ -62,15 +80,19 @@ int main(int argc, char * const argv[]) {
 	argc -= optind;
 	argv += optind;
 
-	if (argc != 1) {
-		warnx("You must supply exactly one serial device node as an argument");
+	if (!node) {
+		warnx("You must specify a valid serial device node");
 		usage();
 	}
 
-	const int fd = open(argv[argc-1], O_RDWR | O_NOCTTY | O_SYNC);
-	configure(fd, baud);
-	send(stdin, fd);
-	close(fd);
+	const int out = open(node, O_RDWR | O_NOCTTY | O_SYNC);
+	configure(out, baud);
+
+	FILE *in = fopen(argv[0], "r");
+	send(in, out);
+	fclose(in);
+
+	close(out);
 
 	return EX_OK;
 }
