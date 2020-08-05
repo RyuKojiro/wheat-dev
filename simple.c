3261f36953aa6eb6ccfed470de69672f02103997
Daniel Loffgren <daniel@loffgren.org>
2020-08-04 17:43:59 -0700

    main can't be static, and the string must be static!
    main needs external linkage to survive all the way to link time.
    The string emits a memcpy when it's not static (which needs to be solved
    in the near future).

--- simple.c
+++ simple.c
@@ -1,7 +1,7 @@
 #include "lcd.h"
 
-static void main() {
-	char string[] = "It's C! ";
+void main() {
+	static char string[] = "It's C! ";
 
 	for (int i = 0; i < sizeof(string); i++) {
 		lcdStorage[i*2] = string[i];
