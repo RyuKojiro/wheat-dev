cf8b5b5baf4512e29f0c265b2a5d851ca905f4ca
Daniel Loffgren <daniel@loffgren.org>
2020-12-26 01:10:05 -0800

    Automatically detect this annoying extra one that occasionally appears on the node name
    …and announce it!

--- relay.sh
+++ relay.sh
@@ -1,1 +1,12 @@
-RELAYDEV=/dev/cu.usbserial-11210
+RELAYDEV="NOT FOUND"
+if [ -e "/dev/cu.usbserial-11210" ]
+then
+	RELAYDEV="/dev/cu.usbserial-11210"
+fi
+
+if [ -e "/dev/cu.usbserial-1210" ]
+then
+	RELAYDEV="/dev/cu.usbserial-1210"
+fi
+
+echo "Relay device is $RELAYDEV"
